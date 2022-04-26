d0f4a3504cb2b8de79e732828800b4e1f42ad499
github_QUENTINBenoit <benquelm@gmail.com>
2022-04-26 22:02:39 +0200

    Initialisation Projet

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+import './app.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+
+    <h1>B'flix</h1>
+  </div>
+);
+
+// == Export
+export default App;
